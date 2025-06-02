4e017da1c4ac685c933cf8c55fab8c9b9f0290b6
SHRIMPTAILS_\shrim <shrimptail.f3@gmail.com>
2025-06-02 20:44:50 +0900

    #42 ENVの項目からSQL非表示表示設定を切り替えるように修正

--- tools/mysql/mysql.go
+++ tools/mysql/mysql.go
@@ -26,6 +26,10 @@ func New() (*MySQL, error) {
 			Colorful:      true,
 		},
 	)
+	// SQL非表示設定か確認する。
+	if os.Getenv("IS_HIDDEN_SQL") == "true" {
+		newLogger = logger.Default.LogMode(logger.Silent)
+	}
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.ExpandEnv("${MYSQL_USER}"), os.ExpandEnv("${MYSQL_PASSWORD}"),
 		os.ExpandEnv("${DB_HOST}"), os.ExpandEnv("${DB_PORT}"),
@@ -45,12 +49,25 @@ func New() (*MySQL, error) {
 
 // NewTest はGORMを使用してMySQLデータベースに接続するための新しいMySQLインスタンスを生成します。
 func NewTest() (*MySQL, error) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	if os.Getenv("IS_HIDDEN_TEST_SQL") == "true" {
+		newLogger = logger.Default.LogMode(logger.Silent)
+	}
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.ExpandEnv("${MYSQL_USER}"), os.ExpandEnv("${MYSQL_PASSWORD}"),
 		os.ExpandEnv("${DB_HOST}"), os.ExpandEnv("${DB_PORT}"),
 		os.ExpandEnv("${MYSQL_TEST_DATABASE}"))
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		log.Println(err)
 		return nil, err
